273dcfe0db81296ab317fdde597338b60bcfb4e2
alfonsodcf <alfonsoservizi01@gmail.com>
2022-04-03 18:43:51 +0200

    dadi:struttura

---
+++ dadi.js
@@ -0,0 +1,28 @@
+// const contenitore = document.getElementById("contenitore");
+
+
+// const button = document.createElement("input");
+// button.setAttribute("type", "button");
+// button.setAttribute("value", "Sfida la cpu con i dadi");
+
+
+    
+// button.addEventListener('click', function(){
+// for (l = 1; l <= 1; l++){
+//     let player = Math.floor(Math.random() * 6) + 1;
+//     let cpu = Math.floor(Math.random() * 6) + 1;
+
+//     console.log(player, cpu);
+
+//     if (player == cpu){
+//         //avete paregiato
+//     }else if (player < cpu){
+//         //la cpu a vinto
+//     } else {
+//         //la cpu a perso
+//     }
+    
+
+// }
+
+// });
